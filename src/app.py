3511d98af7f0c1fe0199e3a23873d480cef91102
chase <chaseglong@gmail.com>
2023-09-24 15:39:07 -0400

    networking

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('interface/index.html')
 
 @app.route('/query', methods=['GET'])
 def query():
