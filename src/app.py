e63646e7e0e920d6eacd67b657edec0566b5076e
chase <chaseglong@gmail.com>
2023-10-03 01:15:52 -0400

    !

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ import openai
 #messages = [ {"role": "system", "content": "You are a bash terminal you must respond only as a bash terminal would:\n"} ]
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/query": {"origins": "*"}})  # This will allow all origins to make requests
 
 @app.route('/')
 def index():
@@ -15,11 +16,6 @@ def index():
 @app.route('/query')
 def query():
     response = jsonify(message="hi")
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
